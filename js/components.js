51e3c80be4a9d65e10bbd63e273db1509a09383c
Mark <mark@digiwig.com>
2017-07-13 07:43:52 +0100

    Draft notification bars component.

--- js/components.js
+++ js/components.js
@@ -2,6 +2,36 @@ function uncheck(element) {
 	element.prop('checked', false);	
 }
 
+/* NOTIFCATION BAR
+************************************************************************/
+
+// example code to bind a notification bar to an action
+var notifyBar = $("#notify"),
+	notifyBtn = $(".notify");
+
+notifyBtn.on("click", function(){ 
+
+	var text = $(this).attr("data-notify");
+	var pos = $(this).attr("data-position");
+	var color = $(this).attr("data-color");
+	if(color) {
+		notifyBar.css("background-color", color);
+	} else {
+		notifyBar.removeAttr("style");
+	}
+
+	notifyBar.removeAttr("class");
+	notifyBar.text(text);
+	setTimeout(function(){ 
+		notifyBar.addClass(pos).addClass("show");
+	}, 10);
+	
+	
+	
+	
+});
+
+
 /* MODAL
 ************************************************************************/
 
